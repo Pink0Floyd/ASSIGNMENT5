fab86cecbf9de928280586daa59c470d98040f76
Filipe Loureiro <filipemloureiro@ua.pt>
2022-06-07 18:07:23 +0100

    Gpio V5.1

--- LedControl/src/gpio.c
+++ LedControl/src/gpio.c
@@ -1,4 +1,3 @@
-
 #include "gpio.h"
 
 /*
@@ -6,20 +5,74 @@
   gpio0_dev=gpio0_init();
 */
 
+static const struct device *gpio0_dev;						    // Pointer to GPIO device structure
+static int gpio0_state=0;							    // The inicialization of the GPIO 0 struct is indicated by this variable
+
+static const struct device *gpio1_dev;						    // Pointer to GPIO device structure
+static int gpio1_state=0;							    // The initialization of the GPIO 1 struct is indicated by this variable    
+	    
+///////////////////////////////////////////////////////////////////////////////////////
+//  Private Functions:
+void gpio0_bind()
+{
+      gpio0_dev=device_get_binding(GPIO0_LABEL);				    // Bind the struct       
+      if(gpio0_dev==NULL)
+      {
+	    printk("Failed to bind to GPIO0\n\r");        
+	    return;
+      }
+      else
+      {
+	    printk("Bind to GPIO0 successfull \n\r");        
+      }    
+
+      if(PRINT_LOOP)
+      {
+	    printk("End of %s test\n\r", gpio0_dev->name);			    // Debbug line, simple test example to check if the pointer it's 
+      }
+     gpio0_state=1;								    // It means that gpio0_device it was already initialised						      
+}
+
+void gpio1_bind()
+{
+      gpio1_dev=device_get_binding(GPIO1_LABEL);				    // Bind the struct       
+      if(gpio1_dev==NULL)
+      {
+	    printk("Failed to bind to GPIO1\n\r");        
+	    return;
+      }
+      else
+      {
+	    printk("Bind to GPIO1 successfull \n\r");        
+      }    
+
+      if(PRINT_LOOP)
+      {    
+	    printk("End of %s test\n\r", gpio1_dev->name);			    // Debbug line, simple test example to check if the pointer it's ok		  
+      }	    
+      gpio1_state=1;								    // It means that gpio0_device it was already initialised
+}
+///////////////////////////////////////////////////////////////////////////////////////
+//  Public Functions:
 struct device *gpio0_init()
 {
-  #define GPIO0_NID DT_NODELABEL(gpio0)                       // Get the gpio0 identifier from the device tree
-  #define GPIO0_LABEL DT_LABEL(GPIO0_NID)                     // Get the gpio0 label from the device tree
-
-  struct device *gpio0_dev;                                    // Pointer to GPIO device structure
-
-  gpio0_dev=device_get_binding(GPIO0_LABEL);                
-  if(gpio0_dev==NULL)
-  {
-    printk("Failed to bind to GPIO0\n\r");        
-  }  
-
-  return gpio0_dev;
+      if(gpio0_state==0)
+      {
+	    gpio0_bind();
+	    if(PRINT_LOOP)
+	    {
+		  printk("GPIO 0 initialised for the first time\n\r");
+	    }
+	    return gpio0_dev;
+      }
+      else
+      {
+	    if(PRINT_LOOP)
+	    {
+		  printk("GPIO 1 already Initialised\n\r");
+	    }
+	    return gpio0_dev;
+      }
 }
 
 
@@ -30,20 +83,23 @@ struct device *gpio0_init()
 
 struct device *gpio1_init()
 {
-  #define GPIO1_NID DT_NODELABEL(gpio1)                       // Get the gpio0 identifier from the device tree
-  #define GPIO1_LABEL DT_LABEL(GPIO1_NID)                     // Get the gpio0 label from the device tree
+      if(gpio1_state==0)
+      {
+	    gpio1_bind();
+	    if(PRINT_LOOP)
+	    {
+		  printk("GPIO 1 initialised for the first time\n\r");
+	    }
+	    return gpio1_dev;
+      }
+      else
+      {
+	     if(PRINT_LOOP)
+	    {
+		  printk("GPIO 1 already Initialised\n\r");
+	    }
+	    return gpio1_dev;
+      }
 
-  struct device *gpio1_dev;                                    // Pointer to GPIO device structure
-
-  gpio1_dev=device_get_binding(GPIO1_LABEL);                
-  if(gpio1_dev==NULL)
-  {
-    printk("Failed to bind to GPIO1\n\r");        
-  }
-  else
-  {
-    printk("Bind to GPIO1 successfull \n\r");        
-  }    
-
-  return gpio1_dev;
+      
 }
--- LedControl/src/gpio.h
+++ LedControl/src/gpio.h
@@ -3,9 +3,9 @@
 * \brief GPIO functions
 *
 * This module implements the functions to initialize and do some gpio configuration
-* \author Filipe Loureiro & Miguel Silva
-* \date 17/05/22
-* \version 1.1
+* \author Filipe Loureiro e Miguel Silva
+* \date 07/06/22
+* \version 5.1
 * \bug No bugs detected
 */
 
@@ -14,34 +14,59 @@
 
 #include "base.h"
 
+#define PRINT_LOOP 1					    // For debug prints
+
+#define GPIO0_NID DT_NODELABEL(gpio0)                       // Get the gpio0 identifier from the device tree
+#define GPIO1_NID DT_NODELABEL(gpio1)                       // Get the gpio0 identifier from the device tree
+
+#define GPIO0_LABEL DT_LABEL(GPIO0_NID)                     // Get the gpio0 label from the device tree
+#define GPIO1_LABEL DT_LABEL(GPIO1_NID)                     // Get the gpio0 label from the device tree
+
 ///////////////////////////////////////////////////////////////////////////////////////
 //  Public Functions:
 
 /**
 * \brief GPIO 0 initialization function
 *
-* Initializes the GPIO 0 in order to use it pins
+* Initializes the GPIO 0 to use the pins
 *
 * \return Return a pointer to a device struct type
-* \date 25/04/22
 * \bug No bugs detected
 */
 
 struct device *gpio0_init();
 
-
 /**
 * \brief GPIO 1 initialization function
 *
-* Initializes the GPIO 1 in order to use it pins
+* Initializes the GPIO 1 to use the pins
 *
 * \return Return a pointer to a struct of device type
-* \date 25/04/22
 * \bug No bugs detected
 */
 struct device *gpio1_init();
 
 ///////////////////////////////////////////////////////////////////////////////////////
 //  Private Functions:
+
+/**
+* \brief GPIO 0 binding function
+*
+*Bind the struct to the device GPIO 0
+*
+* \return Return a pointer to a device struct type
+* \bug No bugs detected
+*/
+void gpio0_bind();
+
+/**
+* \brief GPIO 0 binding function
+*
+*Bind the struct to the device GPIO 1
+*
+* \return Return a pointer to a device struct type
+* \bug No bugs detected
+*/
+void gpio1_bind();
 
 #endif
