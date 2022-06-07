6d83a98146370d4d97aebdb3d9c4be3145dea47b
Filipe Loureiro <filipemloureiro@ua.pt>
2022-06-07 18:07:49 +0100

    Leds V5.1

--- LedControl/src/leds.c
+++ LedControl/src/leds.c
@@ -1,58 +1,69 @@
-
 #include "leds.h"
+#include "gpio.h"
 
-void leds_init(struct device *gpio0_dev, char n_leds, char c)
+static int leds_pin[4]={LED1,LED2,LED3,LED4};								    // Leds data with pins numbers             
+static int leds_pos[4]={1,2,4,8};									    // Leds data with corresponding position, acording to the functions char led
+static const struct device *gpio0_dev;
+
+void leds_init(char n_leds, char c)
 { 
-  int ret=0;                                                                      // Local variable, for errors
-  int i=0; 
+      gpio0_dev=gpio0_init();
+      int ret=0;											    // Local variable, for errors
+      int i=0; 
 
-  if((c=='h') || (c=='H'))                                                        // Initialize the leds to a logical 1
-  {
-    for(i=0; i<4; i++)
-    {
-    //printk("%d\n\r",leds_pos[i]);
-      if((n_leds & leds_pos[i]) == leds_pos[i])
+      if((c=='h') || (c=='H'))										    // Initialize the leds to a logical 1
       {
-        ret=gpio_pin_configure(gpio0_dev, leds_pin[i], GPIO_OUTPUT_ACTIVE);
-        if (ret < 0)
-        {
-          printk("gpio_pin_configure(LED %d) failed with error %d\n\r",(i+1), ret);        
-          return;
-        }
+	    for(i=0; i<4; i++)
+	    {
+		  if(PRINT_LOOP)									    // Debug line, ativated by a Macro
+		  {
+			printk("%d\n\r",leds_pos[i]);
+		  }
+		  if((n_leds & leds_pos[i]) == leds_pos[i])
+		  {
+			ret=gpio_pin_configure(gpio0_dev, leds_pin[i], GPIO_OUTPUT_ACTIVE);
+			if (ret < 0)
+			{
+			      printk("gpio_pin_configure(LED %d) failed with error %d\n\r",(i+1), ret);        
+			      return;
+			}
+		  }
+	    }
       }
-    }
-  }
-  else if((c=='l') || (c=='L'))
-  {
-    for(i=0; i<4; i++)
-    {
-    //printk("%d\n\r",leds_pos[i]);
-      if((n_leds & leds_pos[i]) == leds_pos[i])
+      else if((c=='l') || (c=='L'))
       {
-        ret=gpio_pin_configure(gpio0_dev, leds_pin[i], GPIO_OUTPUT_INACTIVE);
-        if (ret < 0)
-        {
-          printk("gpio_pin_configure(LED %d) failed with error %d\n\r",(i+1), ret);        
-          return;
-        }
+	    for(i=0; i<4; i++)
+	    {
+		  if(PRINT_LOOP)									    // Debug line, ativated by a Macro
+		  {
+			printk("%d\n\r",leds_pos[i]);
+		  }
+		  if((n_leds & leds_pos[i]) == leds_pos[i])
+		  {
+			ret=gpio_pin_configure(gpio0_dev, leds_pin[i], GPIO_OUTPUT_INACTIVE);
+			if (ret < 0)
+			{
+			      printk("gpio_pin_configure(LED %d) failed with error %d\n\r",(i+1), ret);        
+			      return;
+			}
+		  }
+	    }
       }
-    }
-  }
 
 }
 
 
-void leds_set(struct device *gpio0_dev, int n_leds, int state)
+void leds_set(int n_leds, int state)
 {
 
-  int i=0;  
-  for(i=0; i<4; i++)
-  {
-   //printk("%d\n\r",leds_pos[i]);
-    if((n_leds & leds_pos[i]) == leds_pos[i])
-    {
-      gpio_pin_set(gpio0_dev, leds_pin[i], state);
-    }
-  }
+      int i=0;  
+      for(i=0; i<4; i++)
+      {
+	    //printk("%d\n\r",leds_pos[i]);
+	    if((n_leds & leds_pos[i]) == leds_pos[i])
+	    {
+		  gpio_pin_set(gpio0_dev, leds_pin[i], state);
+	    }
+      }
   
 }
