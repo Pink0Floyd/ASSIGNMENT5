4fc3144679d5e2e057a3db9422a8d38b9ca5a476
Pink0Floyd <miguelss@ua.pt>
2022-06-13 20:22:10 +0100

    timer v5.1

---
+++ LedControl/src/timer.h
@@ -0,0 +1,20 @@
+/**
+* \file timer.h
+* \brief Timer module
+*
+* This module contains functions to implement timer functionalities
+*
+* \author Filipe Loureiro & Miguel Silva
+* \date 13/06/22
+* \version 5.1
+* \bug Remains untested
+*/
+
+#ifndef _TIMER_H
+#define _TIMER_H
+
+#include "base.h"
+
+
+
+#endif
