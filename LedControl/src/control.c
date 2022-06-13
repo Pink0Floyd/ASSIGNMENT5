c404680d90accd7d2c23db5990a6d4e56e85acd9
Pink0Floyd <miguelss@ua.pt>
2022-06-13 15:36:03 +0100

    control v5.3

--- LedControl/src/control.c
+++ LedControl/src/control.c
@@ -1,3 +1,36 @@
 
 #include "control.h"
 
+static float prop_error;
+static float int_error;
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Private Member Functions
+
+float saturation(float value, float min,float max)
+{
+	if(value<min)
+	{
+		value=min;
+	}
+	else if(value>max)
+	{
+		value=max;
+	}
+	return value;
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+float controller(float curr,float target)
+{
+	target=saturation(target,MIN_CONTROLLER_VALUE,MAX_CONTROLLER_VALUE);
+	curr=saturation(curr,MIN_CONTROLLER_VALUE,MAX_CONTROLLER_VALUE);
+	prop_error=target-curr;
+	int_error+=prop_error;
+	int_error=int_error*DECAY_FACTOR;
+	int_error=saturation(int_error,MIN_INT_ERROR,MAX_INT_ERROR);
+	float kp=KP*prop_error;
+	float ki=KI*int_error;
+	return (kp+ki);
+}
--- LedControl/src/control.h
+++ LedControl/src/control.h
@@ -5,15 +5,29 @@
 * This module contains functions to implement the controller
 *
 * \author Filipe Loureiro & Miguel Silva
-* \date 12/06/22
-* \version 5.2
+* \date 13/06/22
+* \version 5.3
 * \bug No bugs detected
 */
 
 #include "base.h"
 
+#define DECAY_FACTOR 0.99
+#define MIN_INT_ERROR -12
+#define MAX_INT_ERROR 12
+
+#define MIN_CONTROLLER_VALUE 0
+#define MAX_CONTROLLER_VALUE 100
+
+#define KP 6
+#define KI 2
+
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // Private Member Functions
 
+//float saturation(float value,float min,float max);
+
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // Public Member Functions
+
+void controller(float curr,float target);
