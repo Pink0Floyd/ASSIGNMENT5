6a0253d573d0a5508ca07610416bf712f5af4fec
Pink0Floyd <miguelss@ua.pt>
2022-06-13 20:38:10 +0100

    timer v5.2

--- LedControl/src/timer.c
+++ LedControl/src/timer.c
@@ -1,3 +1,49 @@
 
-#include "adc.h"
+#include "timer.h"
 
+time_data time;
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Private Member Functions
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Public Member Functions
+
+void timer_init()
+{
+	time.sec=0;
+	time.min=0;
+	time.hour=0;
+	time.day=0;
+	printk("\tInitialised timer operation\n");
+}
+
+void update_time(uint8_t sec)
+{
+	update_time_sec(sec);
+}
+
+void update_time_sec(uint8_t sec)
+{
+	
+}
+
+void update_time_min(uint8_t min)
+{
+	
+}
+
+void update_time_hour(uint8_t hour)
+{
+	
+}
+
+void update_time_day(uint8_t day)
+{
+	
+}
+
+void set_time(uint8_t day,uint8_t hour,uint8_t min,uint8_t sec)
+{
+	
+}
--- LedControl/src/timer.h
+++ LedControl/src/timer.h
@@ -6,15 +6,38 @@
 *
 * \author Filipe Loureiro & Miguel Silva
 * \date 13/06/22
-* \version 5.1
+* \version 5.2
 * \bug Remains untested
 */
 
 #ifndef _TIMER_H
 #define _TIMER_H
 
+#define MIN_SEC 0
+#define MAX_SEC 60
+#define MIN_MIN 0
+#define MAX_MIN 60
+#define MIN_HOUR 0
+#define MAX_HOUR 24
+#define MIN_DAY 0
+#define MAX_DAY 365
+
 #include "base.h"
 
+typedef struct time_data
+{
+	uint8_t day;
+	uint8_t hour;
+	uint8_t min;
+	uint8_t sec;
+}time_data;
 
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Private Member Functions
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Public Member Functions
+
+void timer_init();
 
 #endif
