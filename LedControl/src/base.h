5c44f9634d86791c07cd0bd8575a3afd5917e149
Pink0Floyd <miguelss@ua.pt>
2022-06-15 17:18:54 +0100

    base v5.4

--- LedControl/src/base.h
+++ LedControl/src/base.h
@@ -2,11 +2,11 @@
 * \file base.h
 * \brief Base file
 *
-* This file contain the basic includes and macros for general porpouse
+* This file contain the basic includes and macros for general purpose
 *
 * \author Filipe Loureiro & Miguel Silva
-* \date 14/06/22
-* \version 5.2
+* \date 15/06/22
+* \version 5.4
 * \bug No bugs detected
 */
 
@@ -26,9 +26,10 @@
 #include <stdint.h>
 #include <drivers/adc.h>
 #include <hal/nrf_saadc.h>
-#include <drivers/pwm.h>
-//Console use
-#include <console/console.h>
+#include <math.h>
+
+#define INT_SIZE 6
+#define FLOAT_SIZE 3
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // Private Member Functions
