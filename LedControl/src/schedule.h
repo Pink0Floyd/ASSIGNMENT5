9e795fe114cdf3e26155ea98a73219f3ec3b28f2
Pink0Floyd <miguelss@ua.pt>
2022-06-18 20:55:56 +0100

    schedule creation

---
+++ LedControl/src/schedule.h
@@ -0,0 +1,20 @@
+/**
+* \file schedule.h
+* \brief Schedule module
+*
+* This module contains functions to implement a schedule
+*
+* \author Filipe Loureiro & Miguel Silva
+* \date 18/06/22
+* \version 5.0
+* \bug No bugs found
+*/
+
+#ifndef _SCHEDULE_H
+#define _SCHEDULE_H
+
+#include "base.h"
+#include "timer.h"
+
+
+#endif
